8c22ac3ddbff41b402b736ef7163b6343fe12740
laurent <laurent.last7@gmail.com>
2023-08-17 16:58:45 +0200

    #4 add picture

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\PictureRepository;
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(TrickRepository $repository): Response
     {
-        return $this->render('home/index.html.twig');
+        $tricks = $repository->findAll();
+        return $this->render('home/index.html.twig',[
+            'tricks' => $tricks,
+        ]);
     }
 }
