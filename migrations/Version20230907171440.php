918dd541c12e9b1aa13d2e86d0fbae5e2fbe9ece
Laurent Lassallette <laurent.last7@gmail.com>
2023-09-07 19:39:15 +0200

    idea

--- migrations/Version20230907171440.php
+++ migrations/Version20230907171440.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230907093843 extends AbstractMigration
+final class Version20230907171440 extends AbstractMigration
 {
     public function getDescription(): string
     {
