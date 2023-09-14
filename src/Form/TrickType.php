8e29ff46c5cdcd8d875154bc7b4097b2b30e1c1d
laurent <laurent.last7@gmail.com>
2023-09-14 13:16:54 +0200

    Revert "modification done"
    This reverts commit 9e54adfcfd2494427380d9a6f6130d1c432149e1.

---
+++ src/Form/TrickType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Trick;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, ['required' => true,])
+            ->add('pictures', CollectionType::class, [
+                'entry_type' => TrickPictureType::class,
+                'entry_options' => ['label' => false],
+                'label' => false,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'required' => true,
+            ])
+            ->add('videos', CollectionType::class, [
+                'entry_type' => TrickVideoType::class,
+                'entry_options' => ['label' => false],
+                'label' => false,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class, ['required' => true,])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'label',
+                'required' => true,
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => ['class' => 'mt-5 btn btn-primary'],
+            ]);
+    }
+
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Trick::class,
+        ]);
+    }
+
+
+}
